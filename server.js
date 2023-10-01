0bb8d0df8a6d21b2eb402f68054153ad1bbdef61
ahashanahmed222 <ahashanahmed@protonmail.com>
2023-10-02 00:34:27 +0800

    d

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const book = require('./router/book');
 const quran = require('./router/quran');
@@ -12,8 +12,10 @@ dotenv.config()
 mongoose.connect(process.env.APP_SERVER, () => {
   console.log("connected");
 });
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
-app.use(express.json())
+
 app.use(cors())
 
 app.use('/book',book)
